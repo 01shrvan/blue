f4f85e6a0bcfb941ce353fff4d3aff17d77629d0
shrvan <154585738+01shrvan@users.noreply.github.com>
2025-02-25 21:09:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -45,15 +45,15 @@ void printHeart() {
 
 void printLyrics() {
     std::vector<std::tuple<std::string, double, double>> lyrics = {
-        {"It's stuck with you forever, so promise you won't let it go", 0.0, 6.85},
-        {"I'll trust the universe will always bring me to you", 6.85, 8.50},
-        {"I'll imagine we fell in love", 15.35, 2.60},
-        {"I'll nap under moonlight skies with you", 17.95, 3.90},
-        {"I think I'll picture us, you with the waves", 21.85, 3.63},
-        {"The ocean's colors on your face", 25.48, 3.54},
-        {"I'll leave my heart with your air", 29.02, 4.40},
-        {"So let me fly with you", 33.42, 3.41},
-        {"Will you be forever with me?", 36.83, 5.12},
+        {"it's stuck with you forever, so promise you won't let it go", 0.0, 6.85},
+        {"i'll trust the universe will always bring me to you", 6.85, 8.50},
+        {"i'll imagine we fell in love", 15.35, 2.60},
+        {"i'll nap under moonlight skies with you", 17.95, 3.90},
+        {"i think I'll picture us, you with the waves", 21.85, 3.63},
+        {"the ocean's colors on your face", 25.48, 3.54},
+        {"i'll leave my heart with your air", 29.02, 4.40},
+        {"so let me fly with you", 33.42, 3.41},
+        {"will you be forever with me?", 36.83, 5.12},
     };
 
     auto start_time = std::chrono::steady_clock::now();
